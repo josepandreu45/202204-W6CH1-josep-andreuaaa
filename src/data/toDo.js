8dbfbeec28d5bb9e6b6e3ce748088b3f3ee38a9e
josep andreu <josepa45@gmail.com>
2022-05-09 17:49:34 +0200

    Added data to do list

---
+++ src/data/toDo.js
@@ -0,0 +1,19 @@
+const toDo = [
+  {
+    id: 1,
+    name: "poner lavadora",
+    done: false,
+  },
+  {
+    id: 2,
+    name: "pedir cita medico",
+    done: false,
+  },
+  {
+    id: 3,
+    name: "barrer piso",
+    done: false,
+  },
+];
+
+export default toDo;
